e6209f67f3dc24925d56de6522d427d999499820
Daggim 99 <programmerdaggim99@gmail.com>
2022-12-01 13:53:57 +0300

    add the backend url 2

--- frontend/src/redux/features/product/productService.js
+++ frontend/src/redux/features/product/productService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL =
+  "https://inventory-managment-system-daggim-api.onrender.com" ||
+  process.env.REACT_APP_BACKEND_URL;
 
 const API_URL = `${BACKEND_URL}/api/products/`;
 
