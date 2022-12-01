b90a177cd0d6c2f5a7c7fdfd68761684229dce36
Daggim 99 <programmerdaggim99@gmail.com>
2022-12-01 13:43:22 +0300

    add the backend url

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const BACKEND_URL =
+  "https://inventory-managment-system-daggim-api.onrender.com" ||
+  process.env.REACT_APP_BACKEND_URL;
 
 export const validateEmail = (email) => {
   return email.match(
